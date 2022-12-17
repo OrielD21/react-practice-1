6b2088d6697b038050a9b94e2635f5bb34584a9f
Snowy <orieldelgado01@gmail.com>
2022-12-17 17:51:41 -0500

    First and only commit

---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "First Task",
+    description: "My first task",
+  },
+  {
+    id: 1,
+    title: "Second Task",
+    description: "My Second task",
+  },
+  {
+    id: 2,
+    title: "Third Task",
+    description: "My third task",
+  },
+  {
+    id: 3,
+    title: "Fourth Task",
+    description: "My fourth task",
+  },
+];
